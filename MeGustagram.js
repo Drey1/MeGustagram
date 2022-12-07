1a05eec1fc1c299c10ec72942ba82b8917e14542
Drey1 <pjoc91@hotmail.com>
2022-12-07 13:18:03 +0100

    MeGustagram 2.0
    Simple macro script for iMacros that sweeps Instagram pages by liking photos in a natural and organic way to bypass bot filters (For the Spanish version).

---
+++ MeGustagram.js
@@ -0,0 +1,16 @@
+﻿const interval = () => {
+  for (let i = 0; i < numberRepetitions; i++) {
+    iimDisplay("MeGustagram by Drey\n-----------------------------\nRepetition Nº  |  " + (i + 1) + " / " + numberRepetitions);
+    iimPlay("./macros/code.iim");
+  };
+  timer();
+  interval();
+}
+
+const timer = () => {
+  iimDisplay("MeGustagram by Drey\n-----------------------------\nYou must wait 30 minutes...");
+  iimPlay("./macros/clock.iim");
+}
+
+let numberRepetitions = 100;
+interval();
---
+++ README.md
@@ -0,0 +1,5 @@
+	::---------------------------::
+	::| MeGustagram 2.0 by Drey |::
+	::---------------------------::
+
+Simple macro script for iMacros that sweeps Instagram pages by liking photos in a natural and organic way to bypass bot filters (For the Spanish version).
